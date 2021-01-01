c78949199f2178a9dca30bb9defd2ca81a066a30
Michael Alex <michaelaleks.dev@gmail.com>
2021-01-01 12:16:13 +0400

    prettier

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,28 +1,27 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {ReactiveFormsModule, FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
-import {BrowserModule} from '@angular/platform-browser';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {MatSnackBarService} from "./services/mat-snack-bar.service";
+import { MatSnackBarService } from './services/mat-snack-bar.service';
 
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {TokenInterceptor} from "./interceptors/token.interceptor";
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
-import {PageEvent} from "@angular/material";
+import { PageEvent } from '@angular/material';
 
 // components
-import {HeaderComponent} from "./components/header/header.component";
-import {FooterComponent} from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 // dialog components
-import {ChangePasswordDialogComponent} from './components/header/change-password-dialog/change-password-dialog.component';
-import {ConfirmDeletionDialogComponent} from "./components/dialogs/confirm-deletion-dialog/confirm-deletion-dialog.component";
+import { ChangePasswordDialogComponent } from './components/header/change-password-dialog/change-password-dialog.component';
+import { ConfirmDeletionDialogComponent } from './components/dialogs/confirm-deletion-dialog/confirm-deletion-dialog.component';
 
-import {MatModule} from "../mat/mat.module";
-import {NgxSpinnerModule} from "ngx-spinner";
-
+import { MatModule } from '../mat/mat.module';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -32,12 +31,12 @@ import {NgxSpinnerModule} from "ngx-spinner";
 
     // dialog components
     ChangePasswordDialogComponent,
-    ConfirmDeletionDialogComponent
+    ConfirmDeletionDialogComponent,
   ],
   entryComponents: [
     // dialog components
     ChangePasswordDialogComponent,
-    ConfirmDeletionDialogComponent
+    ConfirmDeletionDialogComponent,
   ],
   imports: [
     // modules
@@ -63,14 +62,13 @@ import {NgxSpinnerModule} from "ngx-spinner";
 
     // components
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   providers: [
     // Passing authorization token into every HttpClient request vie HttpOptions headers
-    {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true},
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
     PageEvent,
-    MatSnackBarService
-  ]
+    MatSnackBarService,
+  ],
 })
-export class SharedModule {
-}
+export class SharedModule {}
