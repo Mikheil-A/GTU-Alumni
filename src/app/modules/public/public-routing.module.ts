b9151d4b06c289b6c55124912a8a898eab8ea05b
Michael Alex <michaelaleks.dev@gmail.com>
2021-01-03 15:32:11 +0400

    prevent unaothorized user to access students

--- src/app/modules/public/public-routing.module.ts
+++ src/app/modules/public/public-routing.module.ts
@@ -1,20 +1,16 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import {StudentsComponent} from "./components/students/students.component";
-
-
+import { StudentsComponent } from './components/students/students.component';
+import { AdminGuard } from '../admin/guards/admin.guard';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: 'students'},
-  {path: 'students', component: StudentsComponent}
+  { path: '', pathMatch: 'full', redirectTo: 'students' },
+  { path: 'students', component: StudentsComponent, canActivate: [AdminGuard] },
 ];
-
-
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PublicRoutingModule {
-}
+export class PublicRoutingModule {}
